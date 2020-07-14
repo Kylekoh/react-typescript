9d3a09aacc41875372475a1db934bffb2b335343
고영일 <ykoh728@gmail.com>
2020-07-14 18:01:28 +0900

    add typescript to styled-components theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-const plus = (a: string, b: string): string => a + b;
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
 ReactDOM.render(
-	<React.StrictMode>
+	<ThemeProvider theme={theme}>
 		<App />
-	</React.StrictMode>,
+	</ThemeProvider>,
 	document.getElementById('root')
 );
