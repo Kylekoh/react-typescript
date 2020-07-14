273b56dd02225cbfe97427984267653b7b48dcfc
고영일 <ykoh728@gmail.com>
2020-07-14 17:52:27 +0900

    add styled-components typescript

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.span``;
+const Container = styled.span<{ isBlue: boolean }>`
+	color: ${(props) => (props.isBlue ? 'blue' : 'black')};
+`;
 
 interface IProps {
 	count: number;
